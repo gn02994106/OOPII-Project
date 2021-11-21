9ff8d86bc4bb77cdcc7bc10748794aece7e73d97
gn02994106 <64271432+gn02994106@users.noreply.github.com>
2021-11-22 03:17:51 +0800

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,13 +1,21 @@
 package Project;
 
-import javax.swing.ImageIcon;
-
 public class AI extends Character{
     public AI(Map map, int number){
-        super(map);
-        this.locX = number *50;
-        this.locY = number *50;
-        characterImage = new ImageIcon("Project/Char3.png");
+        super(map, number);
+        switch(number){
+            case 2:
+                locX = map.mapX - 1;
+                locY = 0;
+                break;
+            case 3:
+                locX = 0;
+                locY = map.mapY - 1;
+                break;
+            case 4:
+                locX = map.mapX - 1;
+                locY = map.mapY - 1;
+        }
     }
     public void act(){
     }
